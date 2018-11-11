f591ac07aaff990886d9f6acffb35a97c9ed33b4
msuzuki <led.centipede@gmail.com>
2018-11-12 03:30:46 +0900

    alertを自作、反映

--- 02 url/source-url-util.js
+++ 02 url/source-url-util.js
@@ -36,7 +36,7 @@ javascript:(function(){
       str += arr[2][0] + ": ";
       str += '<a href="' + arr[2][1] + '" target="_blank">' + arr[2][1] + "</a><br>";
       str += arr[4][0] + ": " + arr[4][1] + "<br>" + arr[1][0] + ": " + arr[1][1] + "<br>";
-      alert(str);
+      this.myAlert("tcms-urlutil-diag", str);
     },
     get_page_info_tbl_arr: function() {
       var data = new Array();
@@ -56,7 +56,20 @@ javascript:(function(){
         data[i] = [colname, colval];
       }
       return data;
-    }
+    },
+    myAlert: function(dialogID, txt) {
+			var y = window.pageYOffset;
+			var divcss = 'font-family:\'メイリオ\',Meiryo,sans-serif;font-size:90%;padding:5px;position:absolute;top:' + y + 'px;left:0;background:#fff;border:solid #ccc 1px;z-index:2999;width:600px;height:135px;';
+			var tacss = ' style=\'width:590px; height: 100px; clear: both;\'';
+			var btnfunc = 'this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode);';
+			var panel_start = '<div style=\'padding:3px;background:#eee;height:19px;\'><span style=\'float:left;\'><strong>Dialog</strong></span><a style=\'float:right;\' onclick=\"' + btnfunc + '\">閉じる' + '</a></div><section' + tacss + '>';
+			var panel_end = '</section>';
+			var elm = document.createElement("div");
+			elm.id = dialogID;
+			elm.style.cssText = divcss;
+			elm.innerHTML = panel_start + txt + panel_end;
+			document.getElementsByTagName("body")[0].appendChild(elm);
+		},
   };
   var app = new urlUtil();
   if(app.urlRegx.is_cr_pv_page(app.url)) app.disp_public_url();
